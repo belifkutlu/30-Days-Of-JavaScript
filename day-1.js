8c357315a3a796945a3643303527059ce275af24
belifkutlu <elifkutlu41@gmail.com>
2022-06-20 00:23:36 +0300

    add day-1

---
+++ day-1.js
@@ -0,0 +1,27 @@
+// comments can make code readable
+// Welcome to 30DaysOfJavaScript
+/*
+  comments can make code readable
+  easy to reuse and informative
+*/
+
+// string, boolean, undefined and null data types
+let string = "Elif";
+let boolean = true;
+let undefinedVariable;
+let nullVariable = null;
+
+console.log(typeof string);
+console.log(typeof boolean);
+console.log(typeof undefinedVariable);
+console.log(typeof nullVariable);
+
+let firstName;
+let surname;
+let city;
+let age;
+
+firstName = "Elif";
+surname = "Kutlu";
+city = "Istanbul";
+age = "18";
